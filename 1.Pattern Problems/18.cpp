6bb8fc2d9517746c8c237574b09d6f874f4d682a
tanmayyyy <tanmaykhanna07@gmail.com>
2026-08-20 01:28:56 +0530

    Did pattern problem 18 to 21. Learned further about logic building and did the diamond shaped pattern, empty box pattern and butterfly pattern.

--- 1.Pattern Problems/18.cpp
+++ 1.Pattern Problems/18.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-    
+    int n =5;
+    for(int i =0; i <n; i++){
+        for(int j = 'A' + n-i-1; j < 'A' + n;j++){
+            cout << char(j) << ' ';
+        }
+        cout << endl;
+    }
     return 0;
 }
